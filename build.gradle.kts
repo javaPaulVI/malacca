c968bc92c6345b335ba44f4668a5ae8de59289c0
Paul Hipper <paul@be-hip.eu>
2026-03-21 00:57:14 +0100

    Internal Path-Restructuring

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ publishing {
 
     repositories {
         maven {
-            name = "OSSRH"
+            name = "OSSRH" // this should match <id> in settings.xml if using Maven, not required for Gradle env vars
             url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
                 username = findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
